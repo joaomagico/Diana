9ce87a129d76ae980a365b24b0750b917d3258f8
joaomagico <joao.magico@gmail.com>
2025-08-09 21:57:19 +0100

    add video not restart

--- token-check.js
+++ token-check.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const TOKEN_VALIDO = "abc123";
+  //const TOKEN_VALIDO = "Q15qxgcvQImhc1uDYwUIycyK5L3D01AW5cihRTzr3ViTxuVVWv8F13soryEYcYHV";
   const params = new URLSearchParams(window.location.search);
   const token = params.get("token");
 
@@ -107,4 +108,9 @@ function setGaleria(numFotos, extensao) {
   }
 }
 
+ const video = document.getElementById("meuVideo");
+  
+  video.addEventListener("ended", function() {
+    video.load(); // recarrega o vídeo e mostra o poster
+  });
 
